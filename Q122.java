395e39570761df858b25fdf314f23f0788766271
Nikhil Jha <153512994+nikhiljha9sep@users.noreply.github.com>
2025-08-09 18:47:00 +0530

    Add files via upload

---
+++ Q122.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max=0;
+        int crr_price=0;
+        for(int i=1;i<prices.length;i++)
+        {
+            if(prices[i]>prices[i-1])
+            {
+                int profit=prices[i]-prices[i-1];
+                max=max+profit;
+            }
+            
+        }
+        return max;
+    }
+}
